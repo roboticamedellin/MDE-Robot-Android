fb3afd70c00a8663faf5e76e13bf60ab1e451151
agrappi <alejandro.gomez@rappi.com>
2017-06-20 23:56:11 -0500

    first step

--- app/src/main/java/com/roboticamedellin/robotmde/MainActivity.java
+++ app/src/main/java/com/roboticamedellin/robotmde/MainActivity.java
@@ -2,12 +2,41 @@ package com.roboticamedellin.robotmde;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button ctaLeft;
+
+    boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ctaLeft = (Button) findViewById(R.id.cta_left);
+        textView = (TextView) findViewById(R.id.text_message);
+
+        ctaLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    void setMessage() {
+        if (flag) {
+            textView.setText("Hola");
+        } else {
+            textView.setText("hallo");
+        }
+//        textView.setText(flag ? "Hola" : "Hallo");
+        flag = !flag;
+    }
+
 }
