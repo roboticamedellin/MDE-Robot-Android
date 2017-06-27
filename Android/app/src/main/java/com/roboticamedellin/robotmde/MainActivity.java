360a6db2918435a62310613cf81408b8945cd5a8
Alejandro Gomez <alejandro.gomez@rappi.com>
2017-06-27 10:57:55 -0500

    updated code

--- Android/app/src/main/java/com/roboticamedellin/robotmde/MainActivity.java
+++ Android/app/src/main/java/com/roboticamedellin/robotmde/MainActivity.java
@@ -1,13 +1,45 @@
 package com.roboticamedellin.robotmde;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.text_message)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.cta_left)
+    public void leftClicked() {
+        sendMessage("s-1");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sendMessage("c-1");
+            }
+        }, 250);
+    }
+
+    @OnClick(R.id.cta_right)
+    public void rightClicked() {
+        sendMessage("s-1");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sendMessage("u-1");
+            }
+        }, 250);
+    }
+
 }
